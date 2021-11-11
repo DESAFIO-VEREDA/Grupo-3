2cac8c1322d42de1b46f95bdc7f37455a2a5b7f4
alvesdiiego <diegoalvesribeiro1993@gmail.com>
2021-11-11 12:22:29 -0300

    segundo commit

---
+++ pokemon_api/src/assests/components/header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+
+   
+   
+       
+       return (
+
+
+           <header> 
+              <div>
+          <div className="div-img">
+            <img className="img1" src="Top - Pokédex.png" />
+            <img className="img2" src="pokeball-png.png"/>
+          </div>
+            <div className="div-input">
+
+              <input className="input"
+               type="text" 
+               
+               
+              />
+                 
+              
+             </div>
+            </div>
+            </header>
+          
+       )
+   }
+   export default Header
